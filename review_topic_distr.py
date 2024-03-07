44be8371686f9cf7cdcb192b701c092ded1cc1d3
lbk209 <80578322+lbk209@users.noreply.github.com>
2024-03-07 16:14:19 +0900

    Add files via upload
    multi figs enabled

--- review_topic_distr.py
+++ review_topic_distr.py
@@ -6,63 +6,70 @@ import dash_bootstrap_components as dbc
 import sys, json
 
 
-# Command-Line Arguments: fig files, their path, layout size (default 800x700)
+# Command-Line Arguments: fig files, their path
 arg_options = json.loads(sys.argv[1])
 
 if len(sys.argv) >= 3:
     arg_path = sys.argv[2]
 else:
     arg_path = '.'
-
-if len(sys.argv) == 4:
-    arg_width, arg_height = [int(x) for x in sys.argv[3].split('x')]
-else:
-    arg_width, arg_height = 800, 700
-
+    
 
 # Initialize the app - incorporate a Dash Bootstrap theme
-external_stylesheets = [dbc.themes.FLATLY]
+#external_stylesheets = [dbc.themes.FLATLY]
+external_stylesheets=[dbc.themes.BOOTSTRAP]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
+header = html.Div(
+    [
+        html.Div("Topic Distribution"),
+        dcc.Dropdown(options=arg_options, value=arg_options[0]['value'], id="topics",
+                     multi=True),
+    ],
+)
 
 # App layout
-app.layout = dbc.Container([
-    html.Br(),
-
-    dbc.Row(
-        html.Div('Topic Distribution', className="text-primary text-center fs-5")
-    ),
-
-    dbc.Row(
-        dbc.Col(
-            dcc.Dropdown(options=arg_options, value=arg_options[0]['value'], id="topics"),
-            #width="auto"
-            width=3,
-            ),
-        justify="center"
-    ),
-
-    dbc.Row(
-        dcc.Graph(figure={}, id='topic_distribution')
-    ),
-
-], fluid=True)
+app.layout = dbc.Container(
+    [
+        dbc.Row(dbc.Col(header, lg=6)),
+        dbc.Row(dbc.Col(html.Div(id="graphs"))),
+    ],
+    className="dbc p-4",
+    fluid=True,
+)
 
 
 # Add controls to build the interaction
 @callback(
-    Output(component_id='topic_distribution', component_property='figure'),
+    Output(component_id="graphs", component_property="children"),
     Input(component_id='topics', component_property='value')
 )
-def plot_topic_distr(file):
-    f = f'{arg_path}/{file}'
-    fig = pio.read_json(f)
-    return fig
+def plot_topic_distr(files):
+
+    if not isinstance(files, list):
+        files = [files]
+
+    graphs = []
+    for f in files:
+        f = f'{arg_path}/{f}'
+        fig = pio.read_json(f)
+        g = dcc.Graph(figure=fig, className="border")
+        graphs.append(g)
+
+    layout = []
+    cols = []
+    for i, g in enumerate(graphs, start=1):
+        cols.append(dbc.Col(g))
+        if (i % 2 == 0) or (i == len(graphs)):
+            layout.append(dbc.Row(cols, className="mt-2"))
+            cols = []
+
+    return layout
 
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=False,
-            jupyter_width=arg_width, #"70%"
-            jupyter_height=arg_height, #"70%"
+    app.run(debug=True,
+            #jupyter_width=arg_width, #"70%"
+            #jupyter_height=arg_height, #"70%"
             )
