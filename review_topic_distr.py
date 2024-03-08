16527cfcaf5194efa36b3c290c684ee05ce30e36
lbk209 <80578322+lbk209@users.noreply.github.com>
2024-03-08 16:06:31 +0900

    Add files via upload

--- review_topic_distr.py
+++ review_topic_distr.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, dcc, callback, Output, Input, State, ctx
+from dash import Dash, html, dcc, callback, Output, Input, State
 import plotly.express as px
 import plotly.io as pio
 import dash_bootstrap_components as dbc
@@ -17,37 +17,42 @@ if len(sys.argv) >= 3:
 else:
     arg_path = '.'
 
+# testing
+#arg_options = options
+#arg_path = figs_path
+
+
 # Initialize the app - incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.FLATLY]
 #external_stylesheets=[dbc.themes.BOOTSTRAP]
+
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-header =  html.Div("Topic Distribution")
+title =  html.H4("Topic Distribution", className="text-primary")
 
-dropdown = dcc.Dropdown(options=arg_options, value=arg_options[0]['value'], id="topics",
-                     multi=True)
+dropdown = dcc.Dropdown(options=arg_options, value=arg_options[0]['value'],
+                        id="topics", multi=True)
 
-download = html.Div(
+buttons = dbc.Stack(
     [
         dbc.Button("Save", id='save-button',
-                   color="secondary", className="me-2"),
-        dcc.Upload(dbc.Button("Load", color="secondary", className="me-2"),
+                   color="secondary", className="btn btn-primary btn-sm"),
+        dcc.Upload(dbc.Button("Load", color="secondary", className="btn btn-primary btn-sm"),
                    id='load-topics')
 
-    ], className= "m-1"
+    ],
+    #className= "m-1",
+    direction="horizontal",
+    gap=2
 )
 
 # App layout
 app.layout = dbc.Container(
     [
-        dbc.Row(dbc.Col(header, lg=6)),
-        dbc.Row([dbc.Col(dropdown, lg=6), dbc.Col(download)], align="center"),
-        dbc.Row(dbc.Col(html.Div(id="loaded"))), # testing
+        dbc.Row(dbc.Col(title, lg=6)),
+        dbc.Row([dbc.Col(dropdown, lg=6), dbc.Col(buttons)], align="center"),
         dbc.Row(dbc.Col(html.Div(id="graphs"))),
-        #dbc.Row(dbc.Col(html.Div(id="save-topics"))), # testing
         dcc.Download(id="save-topics"),
-        #dcc.Upload(id='load-topics')
-
     ],
     className="dbc p-4",
     fluid=True,
@@ -60,7 +65,9 @@ app.layout = dbc.Container(
     Input(component_id='topics', component_property='value')
 )
 def plot_topic_distr(files):
-
+    """
+    files: list of json file names to plot
+    """
     if not isinstance(files, list):
         files = [files]
 
@@ -80,27 +87,34 @@ def plot_topic_distr(files):
             cols = []
 
     layout = 'figs: '  + ', '.join(files) # testing
-
     return layout
 
 
 @callback(
-    #Output('save-topics', 'children'),
     Output("save-topics", "data"),
     Input('save-button', 'n_clicks'),
     State('topics', 'value'),
     prevent_initial_call=True
 )
 def save_topics(n_clicks, files):
+    """
+    save file names of choice in dropdown in a txt file
+    """
     if not isinstance(files, list):
         files = [files]
-    layout = ', '.join(files) # testing
-    #return f'{layout}'
-    return dict(content=f'{layout}', filename="topics.txt")
+
+    pattern = r'\d+(?=\.json)'
+    tids = [int(re.findall(pattern, x)[0]) for x in files]
+    tids = '_'.join([str(x) for x in tids])
+    filename = os.path.commonprefix(files) # get common string
+    filename = re.sub(r'\d+$', '', filename) # strip a number suffix
+    filename = f'{filename}_{tids}.txt'
+
+    content = ', '.join(files)
+    return dict(content=f'{content}', filename=filename)
 
 
-@callback(#Output(component_id="loaded", component_property="children"),
-          Output(component_id="topics", component_property="value"),
+@callback(Output(component_id="topics", component_property="value"),
           Input('load-topics', 'contents'),
           State('load-topics', 'filename'),
           State('load-topics', 'last_modified'),
@@ -108,7 +122,7 @@ def save_topics(n_clicks, files):
 def load_topics(contents, filename, date):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
-    files = decoded.decode('utf-8')
+    files = decoded.decode('utf-8') # a string containing json file names
     files = re.findall(r'\w+\.json', files) # list of json files
     return files
 
