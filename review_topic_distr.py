bdeef4a4395b714e0897561afa427e179019af62
lbk209 <80578322+lbk209@users.noreply.github.com>
2024-03-13 16:50:25 +0900

    Add files via upload

--- review_topic_distr.py
+++ review_topic_distr.py
@@ -8,14 +8,18 @@ import io, re, os
 import base64
 import argparse
 
+#from bertopic_custom_util import read_csv
+
 
 # Parsing command-line options and arguments
+"""
 parser = argparse.ArgumentParser()
 parser.add_argument("prfx", help="prefix of fig files")
 parser.add_argument("-d", "--directory", help="path to fig files", default=".")
 parser.add_argument("-p", "--pattern", help="regular expression for fig files", default=r'\d+(?=\.json)')
 parser.add_argument("-jh", "--height", help="layout height", default=650, type=int)
 parser.add_argument("-jw", "--width", help="layout width", default="100%")
+parser.add_argument("-tp", "--topic", help="file of df_topic_info")
 args = parser.parse_args()
 
 fig_prfx = args.prfx
@@ -23,17 +27,29 @@ fig_path = args.directory
 pattern = args.pattern
 jupyter_width = args.width
 jupyter_height = args.height
+df_topic_info = args.topic
 
 debug = False
+"""
 
 # testing
-#fig_path = 'reds2'
-#fig_prfx = 'sdistr'
-#pattern = r'\d+(?=\.json)'
-#debug = True
+fig_path = 'cabs2'
+fig_prfx = 'sdistr'
+pattern = r'\d+(?=\.json)'
+
+jupyter_width = '80%'
+jupyter_height = 800
+df_topic_info = 'df_topic_info.csv'
+debug = True
+
+
+
+line_height = "150%"
+docs_height = "160px"
+
 
 # get json file list
-fig_files = [x for x in os.listdir(fig_path) if x.startswith(fig_prfx)]
+fig_files = [x for x in os.listdir(fig_path) if x.startswith(fig_prfx) and x.endswith('json')]
 fig_files = sorted(fig_files)
 
 n = len(fig_files)
@@ -46,6 +62,23 @@ else:
 # create topic options (dicts of label and value) for dropdown menu
 tids = [int(re.findall(pattern, x)[0]) for x in fig_files]
 options = [{'label':f'Topic {t}', 'value': f} for t, f in zip(tids, fig_files)]
+
+# import df_topic_info
+if df_topic_info is not None:
+    # None if no file exists
+    df_topic_info = read_csv(df_topic_info, path_data=fig_path)
+
+    if df_topic_info is not None:
+        try:
+            cols = df_topic_info.columns.to_list()
+            aspects = cols[cols.index('Representation'):]
+        except ValueError as e:
+            print(f'ERROR: {e}')
+            df_topic_info = None
+
+    topic_info = df_topic_info
+    if topic_info is not None:
+        topic_info = df_topic_info[aspects].map(eval).to_dict(orient='records')
 
 # Initialize the app - incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.FLATLY]
@@ -75,8 +108,11 @@ buttons = dbc.Stack(
 app.layout = dbc.Container(
     [
         dbc.Row(dbc.Col(title, lg=6)),
-        dbc.Row([dbc.Col(dropdown, lg=6), dbc.Col(buttons)], align="center"),
-        dbc.Row(dbc.Col(html.Div(id="graphs"))),
+        #dbc.Row([dbc.Col(dropdown, lg=6), dbc.Col(buttons)], align="center"),
+        #dbc.Row([dbc.Col(dropdown, width=480), dbc.Col(buttons)], align="center"),
+        dbc.Row([dbc.Col(dropdown), dbc.Col(buttons)], align="center"),
+        html.Div(id="graphs"),
+        #[dbc.Row(x) for x in html.Div(id="graphs")], # error
         dcc.Download(id="save-topics"),
     ],
     className="dbc p-4",
@@ -91,27 +127,50 @@ app.layout = dbc.Container(
 )
 def plot_topic_distr(files):
     """
-    files: list of json file names to plot
+    value: list of str combining topic id and json file names to plot
     """
     if not isinstance(files, list):
         files = [files]
 
-    graphs = []
+    layout = []
     for f in files:
         f = f'{fig_path}/{f}'
         fig = pio.read_json(f)
         g = dcc.Graph(figure=fig, className="border")
-        graphs.append(g)
 
-    layout = []
-    cols = []
-    for i, g in enumerate(graphs, start=1):
-        cols.append(dbc.Col(g))
-        if (i % 2 == 0) or (i == len(graphs)):
-            layout.append(dbc.Row(cols, className="mt-2"))
-            cols = []
+        if topic_info is None:
+            row = dbc.Row(dbc.Col(g), className="mt-2")
+        else:
+            tid = int(re.findall(pattern, f)[0])
+            infos = []
+            for title, content in topic_info[tid].items():
+                title_s = html.Span(f'{title}: ', 
+                                    style={'color': '#ABB2B9', 
+                                           #'font-weight': 'bold'
+                                          })
+                if title == 'Representative_Docs':
+                    content = html.Div([html.P(c) for c in content],
+                                        style={
+                                            'fontSize': 14,
+                                            'height': docs_height,
+                                            'overflow':'auto',
+                                            'line-height': line_height,
+                                            'border': '1px solid #D5D8DC',
+                                            #'margin-left': '10px',
+                                            #'margin-right': '10px'
+                                            })
+                    info = [html.Div(title_s), content]
+                else:
+                    content = html.Span(", ".join(content))
+                    info = html.Div([title_s, content], style={'line-height': line_height})
 
-    #layout = 'figs: '  + ', '.join(files) # testing
+                infos.append(html.P(info, className="mb-2"))
+                
+            infos = html.Div(infos, style={"height": fig.layout.height})
+            row = dbc.Row([dbc.Col(g), dbc.Col(infos)], className="mt-2")
+
+        layout.append(row)
+
     return layout
 
 
@@ -156,13 +215,13 @@ def load_topics(contents, filename, date):
     except Exception as e:
         print(e)
         files = no_update
-        
+
     return files, None
 
 
 # Run the app
 if __name__ == '__main__':
     app.run(debug=debug,
-            jupyter_width=jupyter_width, 
+            jupyter_width=jupyter_width,
             jupyter_height=jupyter_height,
             )
